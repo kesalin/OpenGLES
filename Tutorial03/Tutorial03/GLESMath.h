d20e0ccd4c72c557db72499bf0f0c8c9212e4e5d
kesalin <kesalin@gmail.com>
2012-11-29 19:54:00 +0800

    updated math

--- Tutorial03/Tutorial03/GLESMath.h
+++ Tutorial03/Tutorial03/GLESMath.h
@@ -11,6 +11,25 @@
 
 #include <OpenGLES/ES2/gl.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846f
+#endif
+
+#define DEG2RAD( a ) (((a) * M_PI) / 180.0f)
+#define RAD2DEG( a ) (((a) * 180.f) / M_PI)
+
+#define DotProduct(x,y)			((x)[0]*(y)[0]+(x)[1]*(y)[1]+(x)[2]*(y)[2])
+#define VectorSubtract(a,b,c)	((c)[0]=(a)[0]-(b)[0],(c)[1]=(a)[1]-(b)[1],(c)[2]=(a)[2]-(b)[2])
+#define VectorAdd(a,b,c)		((c)[0]=(a)[0]+(b)[0],(c)[1]=(a)[1]+(b)[1],(c)[2]=(a)[2]+(b)[2])
+#define VectorCopy(a,b)			((b)[0]=(a)[0],(b)[1]=(a)[1],(b)[2]=(a)[2])
+#define	VectorScale(v, s, o)	((o)[0]=(v)[0]*(s),(o)[1]=(v)[1]*(s),(o)[2]=(v)[2]*(s))
+#define	VectorMA(v, s, b, o)	((o)[0]=(v)[0]+(b)[0]*(s),(o)[1]=(v)[1]+(b)[1]*(s),(o)[2]=(v)[2]+(b)[2]*(s))
+
+#define VectorClear(a)			((a)[0]=(a)[1]=(a)[2]=0)
+#define VectorNegate(a,b)		((b)[0]=-(a)[0],(b)[1]=-(a)[1],(b)[2]=-(a)[2])
+#define VectorSet(v, x, y, z)	((v)[0]=(x), (v)[1]=(y), (v)[2]=(z))
+#define Vector4Copy(a,b)		((b)[0]=(a)[0],(b)[1]=(a)[1],(b)[2]=(a)[2],(b)[3]=(a)[3])
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -19,6 +38,64 @@ extern "C" {
     {
         GLfloat   m[4][4];
     } KSMatrix4;
+    
+    typedef GLfloat KSVec_t;
+    typedef KSVec_t KSVec2[2];
+    typedef KSVec_t KSVec3[3];
+    typedef KSVec_t KSVec4[4];
+    
+    static inline int VectorCompare( const KSVec3 v1, const KSVec3 v2 )
+    {
+        if (v1[0] != v2[0] || v1[1] != v2[1] || v1[2] != v2[2]) {
+            return 0;
+        }			
+        return 1;
+    }
+    
+    static inline KSVec_t VectorLength( const KSVec3 v )
+    {
+        return (KSVec_t)sqrt (v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+    }
+    
+    static inline KSVec_t VectorLengthSquared( const KSVec3 v )
+    {
+        return (v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+    }
+    
+    static inline KSVec_t Distance( const KSVec3 p1, const KSVec3 p2 ) {
+        KSVec3	v;
+        
+        VectorSubtract (p2, p1, v);
+        return VectorLength( v );
+    }
+    
+    static inline KSVec_t DistanceSquared( const KSVec3 p1, const KSVec3 p2 ) {
+        KSVec3	v;
+        
+        VectorSubtract (p2, p1, v);
+        return v[0]*v[0] + v[1]*v[1] + v[2]*v[2];
+    }
+    
+    static inline void VectorNormalizeFast( KSVec3 v )
+    {
+//        float ilength = Q_rsqrt( DotProduct( v, v ) );
+//        
+//        v[0] *= ilength;
+//        v[1] *= ilength;
+//        v[2] *= ilength;
+    }
+    
+    static inline void VectorInverse( KSVec3 v ){
+        v[0] = -v[0];
+        v[1] = -v[1];
+        v[2] = -v[2];
+    }
+    
+    static inline void CrossProduct( const KSVec3 v1, const KSVec3 v2, KSVec3 cross ) {
+        cross[0] = v1[1]*v2[2] - v1[2]*v2[1];
+        cross[1] = v1[2]*v2[0] - v1[0]*v2[2];
+        cross[2] = v1[0]*v2[1] - v1[1]*v2[0];
+    }
     
     //
     /// multiply matrix specified by result with a scaling matrix and return new matrix in result
